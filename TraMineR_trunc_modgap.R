169ccb263b438d91bf24385dd73837db164b0614
ELICHOS <47089700+ELICHOS@users.noreply.github.com>
2019-02-01 09:26:59 +0100

    Update TraMineR_trunc_modgap.R
    Ajout d'une option si mm <0 ou est nul.

--- TraMineR_trunc_modgap.R
+++ TraMineR_trunc_modgap.R
@@ -17,8 +17,10 @@ TraMineR.trunc.modgap <- function(seqdata, mstate, sl, left = "DEL", right = "DE
   rc <- max(notna.pos)+1
   mm <- na.pos[na.pos > lc+1 & na.pos < rc-1]
   mm.num<-as.numeric(mm)
-  
-  df.gap<-data.frame("mm"=mm, 
+  if(length(mm)==0|is.null(mm)){
+    
+  } else {
+  df.gap<-data.frame("mm"=mm.num, 
              "grouped.gap"=sapply(X = 1:length(mm.num), FUN = function(i){
                if(length(mm.num)<=1) FALSE else {
     if(i==length(mm.num)){
@@ -39,14 +41,14 @@ TraMineR.trunc.modgap <- function(seqdata, mstate, sl, left = "DEL", right = "DE
   })
   )
   df.gap$group<-number <- paste(df.gap$grouped.gap, as.numeric(as.factor(df.gap$grouped.gap)), sep="_")
-  df.gap$lenght.gap<-sapply(1:nrow(df.gap), FUN = function(i){
+  df.gap$length.gap<-sapply(1:nrow(df.gap), FUN = function(i){
     if(df.gap$grouped.gap[i]==TRUE){
       nrow(subset(df.gap, df.gap$group==df.gap$group[i]))
     } else 1
   })
-  subset(df.gap, df.gap$lenght.gap>=minimal.gap)$mm->mm.vraisgaps
-  subset(df.gap, df.gap$lenght.gap<minimal.gap)$mm->mm.fauxgaps
-  
+  subset(df.gap, df.gap$length.gap>=minimal.gap)$mm->mm.vraisgaps
+  subset(df.gap, df.gap$length.gap<minimal.gap)$mm->mm.fauxgaps
+  }
   seqdata.trunc <- seqdata
   
   if (!is.na(left) & lc>0) {
@@ -60,27 +62,30 @@ TraMineR.trunc.modgap <- function(seqdata, mstate, sl, left = "DEL", right = "DE
     else if (right=="NEUTRAL") seqdata.trunc[rc:sl] <- neutral
     else seqdata.trunc[rc:sl] <- right
   }
-  
   if (!is.na(gaps) & length(mm>0)) {
     if (gaps=="DEL") seqdata.trunc[mm.vraisgaps] <- void
     else if (gaps=="NEUTRAL") seqdata.trunc[mm.vraisgaps] <- neutral
-    else seqdata.trunc[mm.vraisgaps] <- gaps
+    else {
+      seqdata.trunc[mm.vraisgaps] <- gaps
+      ####
+      seqdata.trunc2<-sapply(1:length(seqdata.trunc), FUN = function(i){
+        if(i %in% mm.fauxgaps){
+          if(regle.pour.faux.gap=="before"){
+            match(TRUE, !is.na(seqdata.trunc[(i-1):1]))->rank
+            replac.faux.gap<-as.character(seqdata.trunc[i-rank])
+          }
+          if(regle.pour.faux.gap=="after"){
+            match(TRUE, !is.na(seqdata.trunc[(i+1):length(seqdata.trunc)]))->rank
+            replac.faux.gap<-as.character(seqdata.trunc[i+rank])
+          }
+          replac.faux.gap
+        } else seqdata.trunc[i]
+      })
+      names(seqdata.trunc2)<-names(seqdata.trunc)
+      seqdata.trunc2->seqdata.trunc
+    }
   }
-  seqdata.trunc2<-sapply(1:length(seqdata.trunc), FUN = function(i){
-    if(i %in% mm.fauxgaps){
-      if(regle.pour.faux.gap=="before"){
-        match(TRUE, !is.na(seqdata.trunc[(i-1):1]))->rank
-        replac.faux.gap<-as.character(seqdata.trunc[i-rank])
-      }
-      if(regle.pour.faux.gap=="after"){
-        match(TRUE, !is.na(seqdata.trunc[(i+1):length(seqdata.trunc)]))->rank
-        replac.faux.gap<-as.character(seqdata.trunc[i+rank])
-      }
-     replac.faux.gap
-    } else seqdata.trunc[i]
-  })
-  names(seqdata.trunc2)<-names(seqdata.trunc)
-  seqdata.trunc2->seqdata.trunc
+
   ndel <- sum(seqdata.trunc==void, na.rm=TRUE)
   
   if (ndel>0) {
